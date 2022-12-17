40ae38cd602ec8ee2ce3c024ce4427cf9a673639
haiwondaris <hai.nguyen@wondaris.com>
2022-12-17 21:49:26 +0700

    update code lesson-3-exercises

--- src/test/java/com/serenitydojo/checks/DogClassTest.java
+++ src/test/java/com/serenitydojo/checks/DogClassTest.java
@@ -1,7 +1,5 @@
 package com.serenitydojo.checks;
 
-import com.serenitydojo.Dog;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,11 +25,5 @@ public class DogClassTest {
         assertThat(Class.forName("com.serenitydojo.Dog").getDeclaredField("age").getGenericType()).isEqualTo(int.class);
     }
 
-    @Test
-    public void creating_a_dog() {
-        Dog fido = new Dog("Fido", "Bone", 5);
-        Assert.assertEquals(fido.getName(), "Fido");
-        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
-        Assert.assertEquals(fido.getAge(), 5);
-    }
+
 }
